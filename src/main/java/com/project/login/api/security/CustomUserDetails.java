c7857427c53a083d1192d0a8eb51a58da8ca3c49
JianChoi-Kor <wldks79@gmail.com>
2023-11-11 00:36:38 +0900

    @AuthenticationPrincipal Test

--- src/main/java/com/project/login/api/security/CustomUserDetails.java
+++ src/main/java/com/project/login/api/security/CustomUserDetails.java
@@ -32,11 +32,6 @@ public class CustomUserDetails implements UserDetails {
     }
 
     @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
     public String getUsername() {
         return email;
     }
